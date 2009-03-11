001aa17e2878fbb891a23fca342c0439b22e8044
Mathieu Bornoz <mbornoz@ip-10-226-54-102.eu-west-1.compute.internal>
2009-03-11 15:46:35 +0100

    automount devices in large instances

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,4 +2,32 @@ class aws {
   package {"python-boto":
     ensure => present,
   }
+
+  case $ec2_instance_type {
+    "m1.large" : {
+      mount {"/mnt":
+        ensure => absent,
+      }
+
+      file {["/mnt/data1", "/mnt/data2"]:
+        ensure => directory,
+      }
+
+      mount {"/mnt/data1":
+        ensure => mounted,
+        device => "/dev/${ec2_block_device_mapping_ephemeral0}",
+        fstype => "ext3",
+        options => "defaults",
+        require => File["/mnt/data1"],
+      }
+
+      mount {"/mnt/data2":
+        ensure => mounted,
+        device => "/dev/${ec2_block_device_mapping_ephemeral1}",
+        fstype => "ext3",
+        options => "defaults",
+        require => File["/mnt/data2"]
+      }
+    }
+  }
 }
