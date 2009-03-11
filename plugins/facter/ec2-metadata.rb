b1057cd485f2479000a30d575ecde5c77fdeb2ab
Francois Deppierraz <francois.deppierraz@camptocamp.com>
2009-03-11 14:17:45 +0100

    makes ec2 metadata available as facts
    Thanks to Tim Dysinger
    
    http://reductivelabs.com/trac/puppet/wiki/Recipes/AmazonEC2

---
+++ plugins/facter/ec2-metadata.rb
@@ -0,0 +1,38 @@
+# Copyright 2008 Tim Dysinger
+# Distributed under the same license as Facter
+# 27.02.09 KurtBe
+# Added a can_connect? function so that this fact can safely be distributed to non-ec2 instances
+# otherwise the script hangs if the amazon-ip is not reachable
+
+require 'open-uri'
+require 'timeout'
+
+def can_connect?(ip,port,wait_sec=2)
+ Timeout::timeout(wait_sec) {open(ip, port)}
+ return true
+rescue
+  return false
+end
+
+
+def metadata(id = "")
+  open("http://169.254.169.254/2008-02-01/meta-data/#{id||=''}").read.
+    split("\n").each do |o|
+    key = "#{id}#{o.gsub(/\=.*$/, '/')}"
+    if key[-1..-1] != '/'
+      value = open("http://169.254.169.254/2008-02-01/meta-data/#{key}").read.
+        split("\n")
+      value = value.size>1 ? value : value.first
+      symbol = "ec2_#{key.gsub(/\-|\//, '_')}".to_sym
+      Facter.add(symbol) { setcode { value } }
+    else
+      metadata(key)
+    end
+  end
+end
+
+if can_connect?("169.254.169.254",80)
+  metadata
+else
+puts "ec2-metadata not loaded"
+end
