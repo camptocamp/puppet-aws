16c3b7ae0484b18066073e1c0bc6bf3b33c6f735
Mathieu Bornoz <mbornoz@ip-10-226-54-102.eu-west-1.compute.internal>
2009-03-13 10:47:50 +0100

    use /mnt by default for /dev/sdb and /mnt2 for /dev/sdc

--- manifests/classes/partition.pp
+++ manifests/classes/partition.pp
@@ -1,29 +1,27 @@
 class aws::partition {
   case $ec2_instance_type {
     "m1.large" : {
-      mount {"/mnt":
-        ensure => absent,
-      }
 
-      file {["/mnt/data1", "/mnt/data2"]:
+      file {["/mnt","/mnt2"]:
         ensure => directory,
       }
 
-      mount {"/mnt/data1":
+      mount {"/mnt":
         ensure => mounted,
         device => "/dev/${ec2_block_device_mapping_ephemeral0}",
         fstype => "ext3",
         options => "defaults",
-        require => [ Mount["/mnt"], File["/mnt/data1"] ],
+        require => File["/mnt"],
       }
 
-      mount {"/mnt/data2":
+      mount {"/mnt2":
         ensure => mounted,
         device => "/dev/${ec2_block_device_mapping_ephemeral1}",
         fstype => "ext3",
         options => "defaults",
-        require => [ Mount["/mnt"], File["/mnt/data2"] ],
+        require => File["/mnt2"],
       }
+
     }
   }
 }
