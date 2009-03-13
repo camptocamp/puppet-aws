1304bdbebf8f40d241bc3ff8d29b464c292f19fe
Mathieu Bornoz <mathieu.bornoz@camptocamp.com>
2009-03-13 17:38:56 +0100

    added mount for xlarge

--- manifests/classes/partition.pp
+++ manifests/classes/partition.pp
@@ -21,6 +21,45 @@ class aws::partition {
         options => "defaults",
         require => File["/mnt2"],
       }
+    }
+
+    "m1.xlarge" : {
+      
+      file {["/mnt","/mnt2", "/mnt3", "/mnt4"]:
+        ensure => directory,
+      }
+
+      mount {"/mnt":
+        ensure => mounted,
+        device => "/dev/${ec2_block_device_mapping_ephemeral0}",
+        fstype => "ext3",
+        options => "defaults",
+        require => File["/mnt"],
+      }
+
+      mount {"/mnt2":
+        ensure => mounted,
+        device => "/dev/${ec2_block_device_mapping_ephemeral1}",
+        fstype => "ext3",
+        options => "defaults",
+        require => File["/mnt2"],
+      }
+     
+      mount {"/mnt3":
+        ensure => mounted,
+        device => "/dev/${ec2_block_device_mapping_ephemeral2}",
+        fstype => "ext3",
+        options => "defaults",   
+        require => File["/mnt3"],
+      }
+
+      mount {"/mnt4":
+        ensure => mounted,
+        device => "/dev/${ec2_block_device_mapping_ephemeral3}",
+        fstype => "ext3",
+        options => "defaults",   
+        require => File["/mnt4"],
+      }
 
     }
   }
