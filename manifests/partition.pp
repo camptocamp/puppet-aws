ca541db242d0437017c1d49f573988bd29b316d7
Frederic Junod <frederic.junod@camptocamp.com>
2011-12-06 14:56:36 +0100

    aws::partition: refactor code.
    don't depends on the ec2_instance_type to define the mount resources.
    avoid code duplication.

--- manifests/partition.pp
+++ manifests/partition.pp
@@ -5,86 +5,60 @@ class aws::partition {
     default   => 'ext3',
   }
 
-  case $ec2_instance_type {
-    "m1.small", "m2.2xlarge" : {
-    
-      file {"/mnt":
-        ensure => directory,
-        mode   => 755,
-      }
-
-      mount {"/mnt":
-        ensure  => mounted,
-        device  => "/dev/${ec2_block_device_mapping_ephemeral0}",
-        fstype  => $fstype,
-        options => "defaults",
-        require => File["/mnt"],
-      }
+  if ($ec2_block_device_mapping_ephemeral0) {
+    file {'/mnt':
+      ensure => directory,
+      mode   => 755,
     }
-
-    "m1.large" : {
-
-      file {["/mnt","/mnt2"]:
-        ensure => directory,
-        mode   => 755,
-      }
-
-      mount {"/mnt":
-        ensure  => mounted,
-        device  => "/dev/${ec2_block_device_mapping_ephemeral0}",
-        fstype  => $fstype,
-        options => "defaults",
-        require => File["/mnt"],
-      }
-
-      mount {"/mnt2":
-        ensure  => mounted,
-        device  => "/dev/${ec2_block_device_mapping_ephemeral1}",
-        fstype  => $fstype,
-        options => "defaults",
-        require => File["/mnt2"],
-      }
-    }
-
-    "m1.xlarge", "c1.xlarge" : {
-      
-      file {["/mnt","/mnt2", "/mnt3", "/mnt4"]:
-        ensure => directory,
-        mode   => 755,
-      }
-
-      mount {"/mnt":
-        ensure  => mounted,
-        device  => "/dev/${ec2_block_device_mapping_ephemeral0}",
-        fstype  => $fstype,
-        options => "defaults",
-        require => File["/mnt"],
-      }
-
-      mount {"/mnt2":
-        ensure  => mounted,
-        device  => "/dev/${ec2_block_device_mapping_ephemeral1}",
-        fstype  => $fstype,
-        options => "defaults",
-        require => File["/mnt2"],
-      }
-     
-      mount {"/mnt3":
-        ensure  => mounted,
-        device  => "/dev/${ec2_block_device_mapping_ephemeral2}",
-        fstype  => $fstype,
-        options => "defaults",   
-        require => File["/mnt3"],
-      }
-
-      mount {"/mnt4":
-        ensure  => mounted,
-        device  => "/dev/${ec2_block_device_mapping_ephemeral3}",
-        fstype  => $fstype,
-        options => "defaults",   
-        require => File["/mnt4"],
-      }
-
+    mount {'/mnt':
+      ensure  => mounted,
+      device  => "/dev/${ec2_block_device_mapping_ephemeral0}",
+      fstype  => $fstype,
+      options => 'defaults',
+      require => File['/mnt'],
     }
   }
+
+  if ($ec2_block_device_mapping_ephemeral1) {
+    file {'/mnt2':
+      ensure => directory,
+      mode   => 755,
+    }
+    mount {'/mnt2':
+      ensure  => mounted,
+      device  => "/dev/${ec2_block_device_mapping_ephemeral1}",
+      fstype  => $fstype,
+      options => 'defaults',
+      require => File['/mnt2'],
+    }
+  }
+
+  if ($ec2_block_device_mapping_ephemeral2) {
+    file {'/mnt3':
+      ensure => directory,
+      mode   => 755,
+    }
+    mount {'/mnt3':
+      ensure  => mounted,
+      device  => "/dev/${ec2_block_device_mapping_ephemeral2}",
+      fstype  => $fstype,
+      options => 'defaults',
+      require => File['/mnt3'],
+    }
+  }
+
+  if ($ec2_block_device_mapping_ephemeral3) {
+    file {'/mnt4':
+      ensure => directory,
+      mode   => 755,
+    }
+    mount {'/mnt4':
+      ensure  => mounted,
+      device  => "/dev/${ec2_block_device_mapping_ephemeral3}",
+      fstype  => $fstype,
+      options => 'defaults',
+      require => File['/mnt4'],
+    }
+  }
+
 }
